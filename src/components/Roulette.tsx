03c5d7fdd6aa9ebe134bc7ea3b40162ec8fb7739
Mayowa Ayeni <mayhoral008@gmail.com>
2025-02-03 18:17:52 +0100

    added button states and font

--- src/components/Roulette.tsx
+++ src/components/Roulette.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 
-
-const numbers = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
+const numbers = [
+  3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 2, 5, 8, 11, 14, 17, 20, 23, 26,
+  29, 32, 35, 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34,
+];
 
 const blueBgNumbers = [
   3, 9, 12, 18, 21, 27, 30, 36, 5, 14, 17, 23, 32, 1, 7, 16, 19, 25, 34,
 ];
+
 const getNumBgColor = (originalNumber: number) => {
- return blueBgNumbers.find((number) => {
-      return originalNumber === number
+  return blueBgNumbers.find((number) => {
+    return originalNumber === number;
   });
 };
+
 export const NumberBoard = () => {
   return numbers.map((number, index) => {
     return (
       <div
         key={index}
-          className={`h-12  z-40 text-white flex items-center justify-center cursor-pointer lg:w-12 min-w-12  ${getNumBgColor(number)? "bg-[#5856D61A]" : "bg-[#060712]"} bg-[#FFFFFF0A] rounded-lg`}
+        className={`h-12  z-40 text-white flex items-center justify-center cursor-pointer lg:w-12 min-w-12  ${
+          getNumBgColor(number) ? "bg-[#5856D61A]" : "bg-[#060712]"
+        }  rounded-lg`}
         title={number.toString()}
       >
         <span>{number}</span>
